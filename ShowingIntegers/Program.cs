c7b1343c613b69709db40328f52976e2d63496c8
zahidulAlam <zahid.plabon@gmail.com>
2015-05-13 17:41:43 +0600

    1st Ver.

---
+++ ShowingIntegers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowingIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter Lower Limit Number: ");
+            int lowerNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Upper Limit Number: ");
+            int upperNumber = Convert.ToInt32(Console.ReadLine());
+             
+            if(lowerNumber>upperNumber)
+            {
+                Console.WriteLine("Wrong INPUT !!!");
+            }
+            else
+            {
+               Console.WriteLine("The Numbers are : ");
+                for (int i = lowerNumber; i <= upperNumber; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
